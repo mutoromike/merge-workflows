a07aa24c8121c17bbea1dcd079cb7535f7d49464
mutoromike <mikemutoro@gmail.com>
2020-09-01 08:52:18 +0000

    add code in files types: ,.yml,.lock,.md,.ini,.j2,.py,.html,.sh,.txt,.cfg remove code in files types: .yml,,.lock,.py

--- content.py
+++ content.py
@@ -2,15 +2,10 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("/home/strongestavenger/Bitbucket/extraction_services")
+repo = git.Repo("/home/strongestavenger/Github/Tunga/hefaistos-backend")
 # Your mock repo
 mock_repo = git.Repo("/home/strongestavenger/Github/merge-workflows")
 importer = Importer([repo], mock_repo)
 # Use a list of emails if more than one
 importer.set_author(['michaelmukalo@gmail.com', 'mikemutoro@gmail.com'])
-importer.import_repository()
-print("lxkxm")
-print("cljau")
-print("mglxm")
-print("lcvjl")
-print("braep")
+print("pccsa")
