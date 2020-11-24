963ae1c30450108644236e10a2ce0f8aa056bb10
mutoromike <mikemutoro@gmail.com>
2020-11-24 22:39:25 +0000

    add code in files types: .css,.html,.py remove code in files types: .html,.py

--- content.py
+++ content.py
@@ -2,10 +2,10 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("/home/strongestavenger/Github/Tunga/hefaistos-frontend")
+repo = git.Repo("/home/strongestavenger/Bitbucket/web_application_final")
 # Your mock repo
 mock_repo = git.Repo("/home/strongestavenger/Github/merge-workflows")
 importer = Importer([repo], mock_repo)
 # Use a list of emails if more than one
 importer.set_author(['michaelmukalo@gmail.com', 'mikemutoro@gmail.com'])
-importer.import_repository()
+print("vayxx")
