32f7808c9d391d52d21948ab3aeb2dcc12760a40
mutoromike <mikemutoro@gmail.com>
2021-02-22 14:13:59 +0000

    add code in files types: .py remove code in files types:

--- content.py
+++ content.py
@@ -2,10 +2,11 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("/home/strongestavenger/Bitbucket/web_application_final")
+repo = git.Repo("/home/strongestavenger/Bitbucket/extraction_services")
 # Your mock repo
 mock_repo = git.Repo("/home/strongestavenger/Github/merge-workflows")
 importer = Importer([repo], mock_repo)
 # Use a list of emails if more than one
-print("nsxxi")
-print("xcucl")
+importer.set_author(['michaelmukalo@gmail.com', 'mikemutoro@gmail.com'])
+importer.import_repository()
+print("oljda")
